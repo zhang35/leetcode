ce7e3f170783d63d57f711e3df1bb2ffaa741d6e
jiaqi <zhangjqfriend@126.com>
2024-09-09 20:19:18 +0800

    Create 238.product-of-array-except-self.py

---
+++ top_interview_150/238.product-of-array-except-self.py
@@ -0,0 +1,68 @@
+#
+# @lc app=leetcode id=238 lang=python3
+#
+# [238] Product of Array Except Self
+#
+# https://leetcode.com/problems/product-of-array-except-self/description/
+#
+# algorithms
+# Medium (66.65%)
+# Likes:    22856
+# Dislikes: 1445
+# Total Accepted:    2.9M
+# Total Submissions: 4.4M
+# Testcase Example:  '[1,2,3,4]'
+#
+# Given an integer array nums, return an array answer such that answer[i] is
+# equal to the product of all the elements of nums except nums[i].
+# 
+# The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit
+# integer.
+# 
+# You must write an algorithm that runs in O(n) time and without using the
+# division operation.
+# 
+# 
+# Example 1:
+# Input: nums = [1,2,3,4]
+# Output: [24,12,8,6]
+# Example 2:
+# Input: nums = [-1,1,0,-3,3]
+# Output: [0,0,9,0,0]
+# 
+# 
+# Constraints:
+# 
+# 
+# 2 <= nums.length <= 10^5
+# -30 <= nums[i] <= 30
+# The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit
+# integer.
+# 
+# 
+# 
+# Follow up: Can you solve the problem in O(1) extra space complexity? (The
+# output array does not count as extra space for space complexity analysis.)
+# 
+#
+
+# @lc code=start
+# Prefix Product and Suffix Product
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        
+        prefix = [1] * n
+        suffix = [1] * n
+        
+        for i in range(1, n):
+            prefix[i] = prefix[i - 1] * nums[i - 1]
+        
+        for i in range(n - 2, -1, -1):
+            suffix[i] = suffix[i + 1] * nums[i + 1]
+        
+        answer = [prefix[i] * suffix[i] for i in range(n)]
+        
+        return answer
+# @lc code=end
+
