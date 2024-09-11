cc43b362ecb046e0ee3aaca84f9ca18222b26648
jiaqi <zhangjqfriend@126.com>
2024-09-11 18:29:54 +0800

    Create 104.maximum-depth-of-binary-tree.py

---
+++ top_interview_150/104.maximum-depth-of-binary-tree.py
@@ -0,0 +1,87 @@
+#
+# @lc app=leetcode id=104 lang=python3
+#
+# [104] Maximum Depth of Binary Tree
+#
+# https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+#
+# algorithms
+# Easy (76.03%)
+# Likes:    12959
+# Dislikes: 237
+# Total Accepted:    3.4M
+# Total Submissions: 4.4M
+# Testcase Example:  '[3,9,20,null,null,15,7]'
+#
+# Given the root of a binary tree, return its maximum depth.
+# 
+# A binary tree's maximum depth is the number of nodes along the longest path
+# from the root node down to the farthest leaf node.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: root = [3,9,20,null,null,15,7]
+# Output: 3
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: root = [1,null,2]
+# Output: 2
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# The number of nodes in the tree is in the range [0, 10^4].
+# -100 <= Node.val <= 100
+# 
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# BFS
+# class Solution:
+#     def maxDepth(self, root: TreeNode) -> int:
+#         if not root:
+#             return 0
+#         return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+# DFS
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        q = deque()
+        if root:
+            q.append(root)
+
+        level = 0
+        
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+
+        return level
+# @lc code=end
+
