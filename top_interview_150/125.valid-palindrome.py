c7147846c53054b2333301e22058342e086df37c
jiaqi <zhangjqfriend@126.com>
2024-09-11 17:56:17 +0800

    Create 125.valid-palindrome.py

---
+++ top_interview_150/125.valid-palindrome.py
@@ -0,0 +1,79 @@
+#
+# @lc app=leetcode id=125 lang=python3
+#
+# [125] Valid Palindrome
+#
+# https://leetcode.com/problems/valid-palindrome/description/
+#
+# algorithms
+# Easy (48.60%)
+# Likes:    9532
+# Dislikes: 8408
+# Total Accepted:    3.4M
+# Total Submissions: 6.9M
+# Testcase Example:  '"A man, a plan, a canal: Panama"'
+#
+# A phrase is a palindrome if, after converting all uppercase letters into
+# lowercase letters and removing all non-alphanumeric characters, it reads the
+# same forward and backward. Alphanumeric characters include letters and
+# numbers.
+# 
+# Given a string s, return true if it is a palindrome, or false otherwise.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: s = "race a car"
+# Output: false
+# Explanation: "raceacar" is not a palindrome.
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: s = " "
+# Output: true
+# Explanation: s is an empty string "" after removing non-alphanumeric
+# characters.
+# Since an empty string reads the same forward and backward, it is a
+# palindrome.
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= s.length <= 2 * 10^5
+# s consists only of printable ASCII characters.
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            if not s[l].isalnum():
+                l += 1
+            elif not s[r].isalnum():
+                r -= 1
+            elif s[l].lower() == s[r].lower():
+                l += 1
+                r -= 1
+            else:
+                return False
+
+        return True
+# @lc code=end
+
