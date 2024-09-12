4e12acd43924edd3d2e0498fc1c6ad9bcad2b5a0
jiaqi <zhangjqfriend@126.com>
2024-09-12 08:25:49 +0800

    Create 6.zigzag-conversion.py

---
+++ top_interview_150/6.zigzag-conversion.py
@@ -0,0 +1,95 @@
+#
+# @lc app=leetcode id=6 lang=python3
+#
+# [6] Zigzag Conversion
+#
+# https://leetcode.com/problems/zigzag-conversion/description/
+#
+# algorithms
+# Medium (49.14%)
+# Likes:    7827
+# Dislikes: 14839
+# Total Accepted:    1.5M
+# Total Submissions: 3M
+# Testcase Example:  '"PAYPALISHIRING"\n3'
+#
+# The string "PAYPALISHIRING" is written in a zigzag pattern on a given number
+# of rows like this: (you may want to display this pattern in a fixed font for
+# better legibility)
+# 
+# 
+# P   A   H   N
+# A P L S I I G
+# Y   I   R
+# 
+# 
+# And then read line by line: "PAHNAPLSIIGYIR"
+# 
+# Write the code that will take a string and make this conversion given a
+# number of rows:
+# 
+# 
+# string convert(string s, int numRows);
+# 
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: s = "PAYPALISHIRING", numRows = 3
+# Output: "PAHNAPLSIIGYIR"
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: s = "PAYPALISHIRING", numRows = 4
+# Output: "PINALSIGYAHRPI"
+# Explanation:
+# P     I    N
+# A   L S  I G
+# Y A   H R
+# P     I
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: s = "A", numRows = 1
+# Output: "A"
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= s.length <= 1000
+# s consists of English letters (lower-case and upper-case), ',' and '.'.
+# 1 <= numRows <= 1000
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        idx, d = 0, 1
+        rows = [[] for _ in range(numRows)]
+
+        for char in s:
+            rows[idx].append(char)
+            if idx == 0:
+                d = 1
+            elif idx == numRows - 1:
+                d = -1
+            idx += d
+
+        for i in range(numRows):
+            rows[i] = ''.join(rows[i])
+
+        return ''.join(rows)      
+# @lc code=end
+
