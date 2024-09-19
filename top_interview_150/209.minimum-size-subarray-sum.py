dd6919bd97bc097ff53c09177a2ef0efce8a3ef7
jiaqi <zhangjqfriend@126.com>
2024-09-19 18:46:54 +0800

    Create 209.minimum-size-subarray-sum.py

---
+++ top_interview_150/209.minimum-size-subarray-sum.py
@@ -0,0 +1,73 @@
+#
+# @lc app=leetcode id=209 lang=python3
+#
+# [209] Minimum Size Subarray Sum
+#
+# https://leetcode.com/problems/minimum-size-subarray-sum/description/
+#
+# algorithms
+# Medium (47.77%)
+# Likes:    12759
+# Dislikes: 449
+# Total Accepted:    1.2M
+# Total Submissions: 2.4M
+# Testcase Example:  '7\n[2,3,1,2,4,3]'
+#
+# Given an array of positive integers nums and a positive integer target,
+# return the minimal length of a subarray whose sum is greater than or equal to
+# target. If there is no such subarray, return 0 instead.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: target = 7, nums = [2,3,1,2,4,3]
+# Output: 2
+# Explanation: The subarray [4,3] has the minimal length under the problem
+# constraint.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: target = 4, nums = [1,4,4]
+# Output: 1
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+# Output: 0
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= target <= 10^9
+# 1 <= nums.length <= 10^5
+# 1 <= nums[i] <= 10^4
+# 
+# 
+# 
+# Follow up: If you have figured out the O(n) solution, try coding another
+# solution of which the time complexity is O(n log(n)).
+#
+
+# @lc code=start
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        i = j = 0
+        res = 100001
+        total = 0
+        while j < len(nums):
+            total += nums[j]
+            while total >= target:
+                res = min(res, j - i + 1)
+                total -= nums[i]
+                i += 1
+            j += 1
+        return res if res != 100001 else 0
+# @lc code=end
+
