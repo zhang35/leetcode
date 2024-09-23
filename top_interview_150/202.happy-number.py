e2e0b795c2d59e57464f250997f098dfef9efdda
jiaqi <zhangjqfriend@126.com>
2024-09-23 11:04:10 +0800

    Create 202.happy-number.py

---
+++ top_interview_150/202.happy-number.py
@@ -0,0 +1,74 @@
+#
+# @lc app=leetcode id=202 lang=python3
+#
+# [202] Happy Number
+#
+# https://leetcode.com/problems/happy-number/description/
+#
+# algorithms
+# Easy (56.74%)
+# Likes:    10531
+# Dislikes: 1487
+# Total Accepted:    1.6M
+# Total Submissions: 2.8M
+# Testcase Example:  '19'
+#
+# Write an algorithm to determine if a number n is happy.
+# 
+# A happy number is a number defined by the following process:
+# 
+# 
+# Starting with any positive integer, replace the number by the sum of the
+# squares of its digits.
+# Repeat the process until the number equals 1 (where it will stay), or it
+# loops endlessly in a cycle which does not include 1.
+# Those numbers for which this process ends in 1 are happy.
+# 
+# 
+# Return true if n is a happy number, and false if not.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: n = 19
+# Output: true
+# Explanation:
+# 1^2 + 9^2 = 82
+# 8^2 + 2^2 = 68
+# 6^2 + 8^2 = 100
+# 1^2 + 0^2 + 0^2 = 1
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: n = 2
+# Output: false
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= n <= 2^31 - 1
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        s = set()
+        
+        while n != 1 and n not in s:
+            s.add(n)
+            total = 0
+            while n:
+                total += (n % 10) ** 2
+                n //= 10
+            n = total
+        
+        return n == 1
+# @lc code=end
+
