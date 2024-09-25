cdf846b45a40b9e398792de08dacd0a54252fc6a
jiaqi <zhangjqfriend@126.com>
2024-09-25 11:42:52 +0800

    Create 322.coin-change.py

---
+++ top_interview_150/322.coin-change.py
@@ -0,0 +1,72 @@
+#
+# @lc app=leetcode id=322 lang=python3
+#
+# [322] Coin Change
+#
+# https://leetcode.com/problems/coin-change/description/
+#
+# algorithms
+# Medium (44.73%)
+# Likes:    19157
+# Dislikes: 468
+# Total Accepted:    2M
+# Total Submissions: 4.4M
+# Testcase Example:  '[1,2,5]\n11'
+#
+# You are given an integer array coins representing coins of different
+# denominations and an integer amount representing a total amount of money.
+# 
+# Return the fewest number of coins that you need to make up that amount. If
+# that amount of money cannot be made up by any combination of the coins,
+# return -1.
+# 
+# You may assume that you have an infinite number of each kind of coin.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: coins = [1,2,5], amount = 11
+# Output: 3
+# Explanation: 11 = 5 + 5 + 1
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: coins = [2], amount = 3
+# Output: -1
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: coins = [1], amount = 0
+# Output: 0
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= coins.length <= 12
+# 1 <= coins[i] <= 2^31 - 1
+# 0 <= amount <= 10^4
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        min_coins = [amount + 1] * (amount + 1)
+        min_coins[0] = 0
+
+        for i in range(1, amount + 1):
+            for c in coins:
+                if i - c >= 0:
+                    min_coins[i] = min(min_coins[i], 1 + min_coins[i - c])
+        
+        return min_coins[-1] if min_coins[-1] != amount + 1 else -1
+# @lc code=end
+
