a33c9320b1752378c00bdf19ef30b36653952600
jiaqi <zhangjqfriend@126.com>
2024-09-27 16:40:40 +0800

    Create 105.construct-binary-tree-from-preorder-and-inorder-traversal.py

---
+++ top_interview_150/105.construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -0,0 +1,74 @@
+#
+# @lc app=leetcode id=105 lang=python3
+#
+# [105] Construct Binary Tree from Preorder and Inorder Traversal
+#
+# https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
+#
+# algorithms
+# Medium (64.88%)
+# Likes:    15217
+# Dislikes: 524
+# Total Accepted:    1.4M
+# Total Submissions: 2.1M
+# Testcase Example:  '[3,9,20,15,7]\n[9,3,15,20,7]'
+#
+# Given two integer arrays preorder and inorder where preorder is the preorder
+# traversal of a binary tree and inorder is the inorder traversal of the same
+# tree, construct and return the binary tree.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+# Output: [3,9,20,null,null,15,7]
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: preorder = [-1], inorder = [-1]
+# Output: [-1]
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= preorder.length <= 3000
+# inorder.length == preorder.length
+# -3000 <= preorder[i], inorder[i] <= 3000
+# preorder and inorder consist of unique values.
+# Each value of inorder also appears in preorder.
+# preorder is guaranteed to be the preorder traversal of the tree.
+# inorder is guaranteed to be the inorder traversal of the tree.
+# 
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        n = len(preorder)
+        
+        def build(i: int, left: int, right: int) -> Optional[TreeNode]:
+            if i >= n or left > right:
+                return None
+
+            root = TreeNode(preorder[i])
+            k = inorder.index(preorder[i])
+            root.left = build(i + 1, left, k - 1)
+            root.right = build(i + 1 + k - left, k + 1, right)
+
+            return root
+
+        return build(0, 0, n - 1)
+# @lc code=end
+
