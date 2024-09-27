ec4faffd12f6e05fa4f085455c811412c90bb97d
jiaqi <zhangjqfriend@126.com>
2024-09-27 16:40:15 +0800

    Create 92.reverse-linked-list-ii.py

---
+++ top_interview_150/92.reverse-linked-list-ii.py
@@ -0,0 +1,72 @@
+#
+# @lc app=leetcode id=92 lang=python3
+#
+# [92] Reverse Linked List II
+#
+# https://leetcode.com/problems/reverse-linked-list-ii/description/
+#
+# algorithms
+# Medium (48.25%)
+# Likes:    11762
+# Dislikes: 639
+# Total Accepted:    944.6K
+# Total Submissions: 2M
+# Testcase Example:  '[1,2,3,4,5]\n2\n4'
+#
+# Given the head of a singly linked list and two integers left and right where
+# left <= right, reverse the nodes of the list from position left to position
+# right, and return the reversed list.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: head = [1,2,3,4,5], left = 2, right = 4
+# Output: [1,4,3,2,5]
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: head = [5], left = 1, right = 1
+# Output: [5]
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# The number of nodes in the list is n.
+# 1 <= n <= 500
+# -500 <= Node.val <= 500
+# 1 <= left <= right <= n
+# 
+# 
+# 
+# Follow up: Could you do it in one pass?
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        dummy_head = ListNode(-1, head)
+        pre = dummy_head
+
+        for _ in range(left - 1):
+            pre = pre.next
+
+        cur = pre.next
+        for _ in range(right - left):
+            next_node = cur.next
+            cur.next = next_node.next
+            next_node.next = pre.next
+            pre.next = next_node
+
+        return dummy_head.next    
+# @lc code=end
+
