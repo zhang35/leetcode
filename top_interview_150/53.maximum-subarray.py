b5b560893c5dc63ebf992570b33d740a33842598
jiaqi <zhangjqfriend@126.com>
2024-09-28 10:20:41 +0800

    Create 53.maximum-subarray.py

---
+++ top_interview_150/53.maximum-subarray.py
@@ -0,0 +1,65 @@
+#
+# @lc app=leetcode id=53 lang=python3
+#
+# [53] Maximum Subarray
+#
+# https://leetcode.com/problems/maximum-subarray/description/
+#
+# algorithms
+# Medium (51.12%)
+# Likes:    34436
+# Dislikes: 1460
+# Total Accepted:    4.3M
+# Total Submissions: 8.3M
+# Testcase Example:  '[-2,1,-3,4,-1,2,1,-5,4]'
+#
+# Given an integer array nums, find the subarray with the largest sum, and
+# return its sum.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+# Output: 6
+# Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: nums = [1]
+# Output: 1
+# Explanation: The subarray [1] has the largest sum 1.
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: nums = [5,4,-1,7,8]
+# Output: 23
+# Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= nums.length <= 10^5
+# -10^4 <= nums[i] <= 10^4
+# 
+# 
+# 
+# Follow up: If you have figured out the O(n) solution, try coding another
+# solution using the divide and conquer approach, which is more subtle.
+# 
+#
+
+# @lc code=start
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        for i in range(1, len(nums)):
+            nums[i] = max(nums[i-1] + nums[i], nums[i])
+        return max(nums)
+# @lc code=end
+
